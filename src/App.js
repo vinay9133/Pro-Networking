9e9071544a3a030d32619fdc1b009a795db6b5be
Vinay <kommisettivinay07@gmail.com>
2023-01-21 15:15:47 +0530

    intial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import Sidebar from './Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Sidebar.css";
+const Sidebar = () =>{
+    return (
+        <div className="sidebar_top">
+        <img src="https://img.freepik.com/premium-photo/background-two-colors-yellow-dark-pink_280388-1448.jpg?w=2000"></img>
+        <p>Vinayak</p>
+        <h4>vinayakkommisetti@gmail.com</h4>
+        <hr />
+        <div className="sidebar_views">
+        <p>Who Viewed you </p>
+        <p>2300</p></div> 
+        </div>
+             
+    );
+};
+export default Sidebar;
